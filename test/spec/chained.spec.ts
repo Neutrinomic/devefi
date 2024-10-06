b35500550e367e35967e924a8ebdff9fe99241b5
infu <vtsukev@gmail.com>
2024-10-06 18:13:46 +0300

    test diallowed extractors, test longer chain

--- test/spec/chained.spec.ts
+++ test/spec/chained.spec.ts
@@ -46,5 +46,57 @@ describe('Chained vectors', () => {
 
   }, 600 * 1000);
 
+
+
+  it(`throttle->throttle->split`, async () => {
+
+
+    let node = await d.u.createNode({
+      'throttle': {
+        'init': { 'ledger': d.ledgerCanisterId },
+        'variables': {
+          'interval_sec': { 'fixed': 1n },
+          'max_amount': { 'fixed': 10000000n }
+        },
+      },
+    });
+
+
+    let node2 = await d.u.createNode({
+      'throttle': {
+        'init': { 'ledger': d.ledgerCanisterId },
+        'variables': {
+          'interval_sec': { 'fixed': 1n },
+          'max_amount': { 'fixed': 10000000n }
+        },
+      },
+    });
+
+
+    let node3 = await d.u.createNode({
+      'split' : {
+          'init' : {'ledger' : d.ledgerCanisterId},
+          'variables' : {
+              'split' : [50n,50n], 
+            },
+        }
+    });
+
+    await d.u.connectNodes(node.id, 0, node2.id, 0);
+    await d.u.connectNodes(node2.id, 0, node3.id, 0);
+    await d.u.setDestination(node3.id, 0, { owner: d.jo.getPrincipal(), subaccount: [d.u.subaccountFromId(10)] });
+    await d.u.setDestination(node3.id, 1, { owner: d.jo.getPrincipal(), subaccount: [d.u.subaccountFromId(11)] });
+
+
+    await d.u.sendToNode(node.id, 0, 99990000n);
+
+    await d.passTime(50);
+
+
+    expect(await d.u.getLedgerBalance({ owner: d.jo.getPrincipal(), subaccount: [d.u.subaccountFromId(10)] })).toBe(49790000n);
+    expect(await d.u.getLedgerBalance({ owner: d.jo.getPrincipal(), subaccount: [d.u.subaccountFromId(11)] })).toBe(49790000n);
+
+  }, 600 * 1000);
+
 });
 
